cf2d68081c00e87b2276786d2d674f03983a834a
Earljay Juatchon <earljay0606@gmail.com>
2023-09-11 15:30:58 +0800

    ..................

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-center items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex md:justify-center justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img
           src={codelogos}
