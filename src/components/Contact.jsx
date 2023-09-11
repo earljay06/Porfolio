f8a738d9e883081bd5bc85f89e5701bb4be07ffe
Earljay Juatchon <earljay0606@gmail.com>
2023-09-11 17:26:06 +0800

    ...........................

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <form
         method="POST"
         action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
-        className="flex flex-col max-w-[600px] w-full"
+        className="flex flex-col max-w-[600px] w-full "
       >
         <div className="pb-8 mt-24">
           <p className="text-4xl font-bold inline border-b-4 border-amber-500 text-gray-300">
@@ -35,7 +35,7 @@ const Contact = () => {
           placeholder="Message"
         ></textarea>
         <button className="text-white border-2 hover:bg-amber-500 hover:border-amber-500 px-4 py-3 my-8 mx-auto flex items-center">
-          Let's Collaborate
+          Send Message
         </button>
       </form>
     </div>
