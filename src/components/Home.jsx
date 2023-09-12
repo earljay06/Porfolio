e6d373dd7db954cac7ee5a4b0067ecf45ae3103f
Earljay Juatchon <earljay0606@gmail.com>
2023-09-12 15:13:54 +0800

    ..........................

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useRef, useEffect } from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import Typed from "typed.js";
+import Devs4 from '../assets/devs4.png'
+
+
 
 const Home = () => {
   const typedRef = useRef(null);
@@ -29,21 +32,25 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-stone-900">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full lg:pl-0 md:pl-40">
-        <p className="text-amber-500 font-bold text-2xl">Hi, This is</p>
-        <h1 className="text-2xl lg:text-5xl md:text-2xl font-bold text-[#ccd6f6] mt-4">
-          Earl Jay Juatchon
-        </h1>
-        <h2 className="text-xl lg:text-5xl md:text-2xl font-bold text-[#8892b0]">
-          I'am a <span className="auto-input"></span>
-        </h2>
-        <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-amber-500 hover:border-amber-500 mt-8">
-            Let's Talk
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
-        </div>
+     
+          <p className="text-amber-500 font-bold text-2xl">Hi, This is</p>
+          <h1 className="text-2xl lg:text-5xl md:text-2xl font-bold text-[#ccd6f6] mt-4">
+            Earl Jay Juatchon
+          </h1>
+          <h2 className="text-xl lg:text-5xl md:text-2xl font-bold text-[#8892b0]">
+            I'am a <span className="auto-input"></span>
+          </h2>
+          <div>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-amber-500 hover:border-amber-500 mt-8">
+              Let's Talk
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </div>
+       
+       
+        
       </div>
     </div>
   );
