871e60b3afe0ee4c11fdff975d392991c306dc65
Earljay Juatchon <earljay0606@gmail.com>
2023-10-19 22:43:53 +0800

    ....

--- src/data/datas.js
+++ src/data/datas.js
@@ -5,32 +5,32 @@ import Favewebsite from '../assets/project/favewebsite.png';
 export const data = [
   {
     id: 1,
-    name: "First Projects",
+    name: "First Projects in My Company this is Static Website.",
     image: Favewebsite,
     live: "https://fave-website.vercel.app/",
   },
-  {
-    id: 1,
-    name: "Second Projects",
-    image: WorkIm,
-    live: "",
-  },
-  {
-    id: 2,
-    name: "Third Projects",
-    image: RealEst,
-    live: "",
-  },
-  {
-    id: 3,
-    name: "Fourt Projects",
-    image: WorkIm,
-    live: "",
-  },
-  {
-    id: 4,
-    name: "Fifth Projects",
-    image: RealEst,
-    live: "",
-  },
+  // {
+  //   id: 1,
+  //   name: "Second Projects",
+  //   image: WorkIm,
+  //   live: "",
+  // },
+  // {
+  //   id: 2,
+  //   name: "Third Projects",
+  //   image: RealEst,
+  //   live: "",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Fourt Projects",
+  //   image: WorkIm,
+  //   live: "",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Fifth Projects",
+  //   image: RealEst,
+  //   live: "",
+  // },
 ];
